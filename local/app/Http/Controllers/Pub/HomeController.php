4f9e87fba90e96e32df5516c2f3b65d71d27bd67
boyvn2911 <boyvn2911@gmail.com>
2018-08-18 14:02:59 +0700

    maintainance view

--- local/app/Http/Controllers/Pub/HomeController.php
+++ local/app/Http/Controllers/Pub/HomeController.php
@@ -16,11 +16,15 @@ class HomeController extends Controller
 {
     public function getIndex()
     {
-        dd("chao em");
+        return view('public.maintain');
+    }
+
+    public function getHome()
+    {
         $data['hot_homestay'] = HomeStay::where('homestay_active',1)->orderBy('homestay_id','desc')->take(9)->get();
         $data['blogs'] = DB::table('articel')->orderBy('id','desc')->take(9)->get();
         $data['comments'] = Comment::with('user')->with('homestay')->take(9)->get();
-    	return view('public.index',$data);
+        return view('public.index',$data);
     }
 
     public function getBlogs()
