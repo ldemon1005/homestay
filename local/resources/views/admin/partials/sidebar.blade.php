0df234e24d54fa64b6b9a9b36a11d821b451256b
boyvn2911 <boyvn2911@gmail.com>
2018-09-27 10:13:15 +0700

    son

--- local/resources/views/admin/partials/sidebar.blade.php
+++ local/resources/views/admin/partials/sidebar.blade.php
@@ -8,19 +8,19 @@
             </div>
             <div class="pull-left info">
                 <p>{{ Auth::guard('admin')->user()->name ?? 'Admin' }}</p>
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+                {{--<a href="#"><i class="fa fa-circle text-success"></i> Online</a>--}}
             </div>
         </div>
         <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search...">
-                <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-            </div>
-        </form>
+        {{--<form action="#" method="get" class="sidebar-form">--}}
+            {{--<div class="input-group">--}}
+                {{--<input type="text" name="q" class="form-control" placeholder="Search...">--}}
+                {{--<span class="input-group-btn">--}}
+                {{--<button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>--}}
+                {{--</button>--}}
+              {{--</span>--}}
+            {{--</div>--}}
+        {{--</form>--}}
         <!-- /.search form -->
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
@@ -75,12 +75,12 @@
                 </a>
             </li>
 
-            <li>
-                <a href="pages/widgets.html">
-                    <i class="fa fa-th"></i> <span>Widgets</span>
-                    <span class="pull-right-container"><small class="label pull-right bg-green">new</small></span>
-                </a>
-            </li>
+            {{--<li>--}}
+                {{--<a href="pages/widgets.html">--}}
+                    {{--<i class="fa fa-th"></i> <span>Widgets</span>--}}
+                    {{--<span class="pull-right-container"><small class="label pull-right bg-green">new</small></span>--}}
+                {{--</a>--}}
+            {{--</li>--}}
             {{--<li class="header">LABELS</li>--}}
             {{--<li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>--}}
             {{--<li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>--}}
