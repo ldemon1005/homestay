7a0d010c8bba31d95b00f24ec626b5c1b4213a5b
boyvn2911 <boyvn2911@gmail.com>
2018-10-02 16:54:45 +0700

    Merge branch 'tuan_1' of https://github.com/ldemon1005/homestay into son
    # Conflicts:
    #	local/.env

--- local/resources/views/public/index.blade.php
+++ local/resources/views/public/index.blade.php
@@ -26,19 +26,16 @@
             autoplayTimeout:2000,
             autoplayHoverPause:true,
 			responsiveClass:true,
+            nav:false,
             responsive:{
                 0:{
-                    items:1,
-                    nav:false
+                    items:1
                 },
                 600:{
-                    items:2,
-                    nav:false
+                    items:2
                 },
                 1000:{
-                    items:3,
-                    nav:false,
-                    loop:false
+                    items:3
                 }
             }
         });
